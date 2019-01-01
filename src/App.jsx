17727aab02a6ed04ef20335f3a960d811e040bc3
Nurseiit Abdimomyn <nurseyit.abdimomyn@gmail.com>
2019-01-01 22:45:11 +0900

    File type change && MyMap Component added

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import MyMap from "./components/MyMap";
+
+class App extends Component {
+  render() {
+    return <MyMap />;
+  }
+}
+
+export default App;
