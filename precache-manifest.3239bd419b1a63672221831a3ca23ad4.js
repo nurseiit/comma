112db3d9d6b18de6ff2bbd100ac3e40dc7331920
Nurseiit Abdimomyn <nurseyit.abdimomyn@gmail.com>
2019-01-01 17:25:27 +0900

    Updates

---
+++ precache-manifest.3239bd419b1a63672221831a3ca23ad4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e990e90bf908d61e30c2",
+    "url": "/comma/static/css/main.c42a7504.chunk.css"
+  },
+  {
+    "revision": "e990e90bf908d61e30c2",
+    "url": "/comma/static/js/main.e990e90b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/comma/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1fc4a88969376235df0f",
+    "url": "/comma/static/js/runtime~main.1fc4a889.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/comma/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f69b390582fee5c60f51e1f0ac7bb81e",
+    "url": "/comma/index.html"
+  }
+];
