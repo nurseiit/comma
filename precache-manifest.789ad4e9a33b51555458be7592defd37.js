30fe032c999210f5409ac24c2afd2a16a67c3862
Nurseiit Abdimomyn <nurseyit.abdimomyn@gmail.com>
2019-01-04 16:43:39 +0900

    Updates

---
+++ precache-manifest.789ad4e9a33b51555458be7592defd37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c5ebf2e351e4273146f",
+    "url": "/comma/static/js/main.4c5ebf2e.chunk.js"
+  },
+  {
+    "revision": "5e6897718c02433e1e67",
+    "url": "/comma/static/css/1.391eba0b.chunk.css"
+  },
+  {
+    "revision": "5e6897718c02433e1e67",
+    "url": "/comma/static/js/1.5e689771.chunk.js"
+  },
+  {
+    "revision": "1fc4a88969376235df0f",
+    "url": "/comma/static/js/runtime~main.1fc4a889.js"
+  },
+  {
+    "revision": "a3fb75a3afdb0ad30a94d7494e09eb18",
+    "url": "/comma/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/comma/precache-manifest.2b923464197a61bb4b8df78cc99d24a7.js"
+  "/comma/precache-manifest.789ad4e9a33b51555458be7592defd37.js"
 );
 
 workbox.clientsClaim();
